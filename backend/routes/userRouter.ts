7a01852d21af500799c5d3023e9819ef7f64432a
Valeriia Lyakisheva <valeriia.lyakisheva@bahag.com>
2024-11-18 16:08:47 +0100

    User Roter changed

--- backend/routes/userRouter.ts
+++ backend/routes/userRouter.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, Router } from "express";
-import { getUserOrders } from "../controllers/orderController"
-import { authenticateJWT } from '../middleware/jwtMiddleware'
+import { getUserOrders } from "../controllers/orderController";
+import { authenticateJWT } from "../middleware/jwtMiddleware";
 import {
   deleteUser,
   getAllUsers,
@@ -11,11 +11,10 @@ import { checkAdmin } from "../middleware/checkAdmin";
 
 const userRouter: Router = Router();
 
-userRouter.get("/users", authenticateJWT, checkAdmin, getAllUsers);
+userRouter.get("/users", checkAdmin, getAllUsers);
 userRouter.get("/users/orders", authenticateJWT, getUserOrders);
 userRouter.get("/users/:id", authenticateJWT, getUserByID);
 userRouter.put("/users/:id", authenticateJWT, updateUser);
 userRouter.delete("/users/:id", authenticateJWT, deleteUser);
-
 
 export default userRouter;
