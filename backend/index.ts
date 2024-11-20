5ea78730ab74ddbbfc61c9b0b53c575d5b61fa93
Valeriia Lyakisheva <valeriia.lyakisheva@bahag.com>
2024-11-20 16:31:00 +0100

    change route

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,12 @@
-import express, {Application} from 'express';
-import { AppDataSource } from './data/dataSource';
+import express, { Application } from "express";
+import { AppDataSource } from "./data/dataSource";
 import authRouter from "./routes/authRouter";
-import userRouter from "./routes/userRouter"
-import productRouter from './routes/productRouter'
-import orderRouter from './routes/orderRouter'
-import cors from 'cors'
-import 'dotenv/config'
-import 'reflect-metadata';
+import userRouter from "./routes/userRouter";
+import productRouter from "./routes/productRouter";
+import orderRouter from "./routes/orderRouter";
+import cors from "cors";
+import "dotenv/config";
+import "reflect-metadata";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -14,15 +14,16 @@ const port = process.env.PORT || 3000;
 // Middleware for parsing JSON requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 AppDataSource.initialize()
   .then(() => {
-    app.use('/api', authRouter, userRouter, productRouter, orderRouter)
-    app.listen(port, () => console.log(`Welcome to our server running on port ${port} 🟢`))
-    console.log('Database connected');
+    app.use(authRouter, userRouter, productRouter, orderRouter);
+    app.listen(port, () =>
+      console.log(`Welcome to our server running on port ${port} 🟢`)
+    );
+    console.log("Database connected");
   })
   .catch((error) => {
-    console.log(error)
+    console.log(error);
   });
-  
